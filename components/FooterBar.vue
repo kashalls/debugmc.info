8a1624f2e57c34a4961ac81d651bdd0a7750d2b4
Kashalls <kashalls@pm.me>
2022-09-17 16:02:22 -0700

    Remove footerbar ip typer

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -2,24 +2,6 @@
 <template>
   <footer class="footer">
     <div class="content has-text-centered">
-      <p v-if="$fetchState.pending">
-        Awaiting data...
-      </p>
-      <p v-else-if="$fetchState.error">
-        Failed to check data.
-      </p>
-      <p v-else>
-        <b>Your IP:</b> <b-tag rounded>
-          {{ ip.ip }}
-        </b-tag> · <b>ISP:</b> <b-tag rounded>
-          {{ ip.connection.isp }}
-        </b-tag> <b-tag rounded>
-          ASN {{ ip.connection.asn }}
-        </b-tag>
-        · <b>Status:</b> <b-tag rounded>
-          Residential
-        </b-tag>
-      </p>
       <p>
         <a href="https://debugmc.info">Debugmc.info</a> created by <a href="https://github.com/kashalls">Kashall</a> · Source available on <a href="https://github.com/Kashalls/debugmc.info">            <b-icon
           icon="github"
@@ -35,16 +17,6 @@
 
 <script>
 export default {
-  fetchOnServer: false,
-  data () {
-    return {
-      ip: {}
-    }
-  },
-  async fetch () {
-    if (process.client) {
-      this.ip = await this.$axios.$get('http://ipwho.is/')
-    }
-  }
+  fetchOnServer: false
 }
 </script>
