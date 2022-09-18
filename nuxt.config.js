aa76cb1849613473d45d4bffd9c7cf6f3fb0f311
Kashalls <kashalls@pm.me>
2022-09-18 15:41:02 -0700

    Custom cloudflare colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/minecraft.css'
+    '~/assets/minecraft.css',
+    '~/assets/custom-colors.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
