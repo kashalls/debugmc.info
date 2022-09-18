dac13010a0255e04ce12891cb816cbb943c1e74b
Kashalls <kashalls@pm.me>
2022-09-17 18:03:31 -0700

    Revert "Unnecessary assets"
    This reverts commit dfc7d3304d1082639c62b11e9b8561e1b593ed08.

---
+++ components/ServerTag.vue
@@ -0,0 +1,68 @@
+<template>
+  <b-tag
+    class="is-clickable is-unselectable"
+    :attached="isAttached"
+    :closable="closable"
+    :icon="getIcon"
+    :icon-type="getIconType"
+    size="is-small"
+    @click="handleClick"
+    @close="handleClose"
+  >
+    {{ content }}
+  </b-tag>
+</template>
+
+<script>
+export default {
+  props: {
+    suggested: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    content: {
+      type: String,
+      required: true,
+      default: ' '
+    },
+    closable: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    getIcon () {
+      const { icon, suggested } = this
+      if (suggested) {
+        return 'star'
+      }
+
+      return icon
+    },
+    getIconType () {
+      if (this.suggested) {
+        return 'is-warning'
+      }
+      return ''
+    },
+    isAttached () {
+      return (this.suggested || this.icon) || this.closable
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$emit('click', this.content)
+    },
+    handleClose () {
+      this.$emit('close', this.content)
+    }
+  }
+}
+</script>
