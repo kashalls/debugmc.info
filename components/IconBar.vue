ebfaba1be6066fe89131367276e7bb13aea7e213
Kashalls <kashalls@pm.me>
2022-09-17 18:03:41 -0700

    Add IconBar for future use

---
+++ components/IconBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="block">
+    <b-tooltip
+      v-for="service in services"
+      :key="service"
+    >
+      <FontAwesomeIcon
+        :icon="service"
+      />
+      <template #label>
+        This domain is using <u>{{ service }}</u>.
+      </template>
+    </b-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    services: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  }
+}
+</script>
