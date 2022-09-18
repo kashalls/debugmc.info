00a339700d95cb96d9a9c357994c81041d548aa4
Kashalls <kashalls@pm.me>
2022-09-18 15:41:12 -0700

    Page meta update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,31 @@
+const image = 'https://debugmc.info/icon.png'
+const description = 'Easily debug your minecraft server with easy to use tools! Find information on your server like ping, version, player online and even dns records! Supporting all protocol versions and platforms!'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'debugmc',
+    title: 'debugmc.info',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: description },
+      /* Twitter */
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@kashalls' },
+      { hid: 'twitter:creator', name: 'twitter:creator', content: '@kashalls' },
+      { hid: 'twitter:title', name: 'twitter:title', content: 'debugmc.info' },
+      { hid: 'twitter:description', name: 'twitter:description', content: description },
+      { hid: 'twitter:image', name: 'twitter:image', content: image },
+      /* Open-Graph */
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'debugmc.info' },
+      { hid: 'og:description', name: 'og:description', content: description },
+      { hid: 'og:image', name: 'og:image', content: image },
+      /* Others */
+      { hid: 'theme-color', name: 'theme-color', content: '#171717' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
